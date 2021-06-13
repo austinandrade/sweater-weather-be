4aee709a0c6856f861aeeed79788c30791f7d7bb
austinandrade <austinmandrade@gmail.com>
2021-06-13 01:04:59 -0400

    Update namespace from weather to forecast

--- app/controllers/api/v1/weather_controller.rb
+++
@@ -1,5 +0,0 @@
-class Api::V1::WeatherController < ApplicationController
-  def show
-    
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :weather, only: :show
+      resources :forecast, only: :show
     end
   end
 end
