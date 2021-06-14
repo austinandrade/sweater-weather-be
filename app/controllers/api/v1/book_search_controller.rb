3702b30aedfeb866602d6798aec036bd3a649075
austinandrade <austinmandrade@gmail.com>
2021-06-14 12:37:10 -0400

    Create facad calls and desired book object serialization

---
+++ app/controllers/api/v1/book_search_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::BookSearchController < ApplicationController
+  def index
+    location_coordinates = LocationFacade.get_lat_long(params[:location])
+    current_forecast = ForecastFacade.get_forecast(location_coordinates.latitude, location_coordinates.longitude).current_weather
+    found_books = BookFacade.get_books(params[:location], params[:quantity], current_forecast, params[:location])
+    render json: BooksSerializer.new(found_books)
+  end
+end
