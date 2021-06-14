0c3245ae4662475ebbb7c20706f4c402293ec026
austinandrade <austinmandrade@gmail.com>
2021-06-14 19:39:01 -0400

    Create photo poro

---
+++ app/poros/photo.rb
@@ -0,0 +1,22 @@
+class Photo
+  attr_reader :id
+  def initialize(data, location)
+    @id = nil
+    @data = data
+    @location = location
+    @image = image
+  end
+
+  def image
+    {
+      location:  @location,
+      image_url: @data[:results][0][:urls][:raw],
+      credit:
+      {
+        source: "https://unsplash.com",
+        photographer: @data[:results][0][:tags][0][:source][:cover_photo][:user][:name],
+        photographer_profile: @data[:results][0][:tags][0][:source][:cover_photo][:user][:links][:html]
+      }
+    }
+  end
+end
