061360b3b5e9fb66b34942fd5d3473af23d6aed3
austinandrade <austinmandrade@gmail.com>
2021-06-14 13:22:02 -0400

    Remove redundant location mention

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::BookSearchController < ApplicationController
   def index
     location_coordinates = LocationFacade.get_lat_long(params[:location])
     current_forecast = ForecastFacade.get_forecast(location_coordinates.latitude, location_coordinates.longitude).current_weather
-    found_books = BookFacade.get_books(params[:location], params[:quantity], current_forecast, params[:location])
+    found_books = BookFacade.get_books(params[:location], params[:quantity], current_forecast)
     render json: BooksSerializer.new(found_books)
   end
 end
--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -1,13 +1,13 @@
 class BookFacade
   class << self
-    def get_books(location, limit, current_weather, destination)
-      create_books(BookService.get_book_data(location), current_weather, limit, destination)
+    def get_books(location, limit, current_weather)
+      create_books(BookService.get_book_data(location), current_weather, limit, location)
     end
 
     private
 
-    def create_books(book_data, current_weather, limit, destination)
-      Book.new(book_data, current_weather, limit, destination)
+    def create_books(book_data, current_weather, limit, location)
+      Book.new(book_data, current_weather, limit, location)
     end
   end
 end
