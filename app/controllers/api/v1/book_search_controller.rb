03bd79db5fb82676460b4d4158b6f42a35c2868b
austinandrade <austinmandrade@gmail.com>
2021-06-14 13:45:07 -0400

    Flash error when missing params or passing negative quantity params

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::BookSearchController < ApplicationController
   def index
-    location_coordinates = LocationFacade.get_lat_long(params[:location])
-    current_forecast = ForecastFacade.get_forecast(location_coordinates.latitude, location_coordinates.longitude).current_weather
-    found_books = BookSearchFacade.get_books(params[:location], params[:quantity], current_forecast)
-    render json: BooksSerializer.new(found_books)
+    if params[:location] && params[:quantity].to_i > 0
+      location_coordinates = LocationFacade.get_lat_long(params[:location])
+      current_forecast = ForecastFacade.get_forecast(location_coordinates.latitude, location_coordinates.longitude).current_weather
+      found_books = BookSearchFacade.get_books(params[:location], params[:quantity], current_forecast)
+      render json: BooksSerializer.new(found_books)
+    else
+      render json: { error: 'Please include location & non-negative quantity params' }.to_json, status: 400
+    end
   end
 end
