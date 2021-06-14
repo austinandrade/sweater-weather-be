d8c7d3f1b1564019b0aa473caa9250b9dc258af1
austinandrade <austinmandrade@gmail.com>
2021-06-14 19:38:06 -0400

    Test background show page happy and sad path

---
+++ spec/requests/api/v1/backgrounds/show_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe 'backgrounds show' do
+  describe 'backgrounds requests' do
+    it 'renders a single background based on passed location', :vcr do
+      params = "denver, co"
+      get "/api/v1/backgrounds?location=#{params}"
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(response.server_error?).to eq(false)
+
+      photo_info = JSON.parse(response.body, symbolize_names: true)
+
+      expect(photo_info).to have_key(:data)
+      expect(photo_info[:data]).to be_a(Hash)
+
+      expect(photo_info[:data]).to have_key(:id)
+      expect(photo_info[:data][:id]).to eq(nil)
+
+      expect(photo_info[:data]).to have_key(:type)
+      expect(photo_info[:data][:type]).to eq("image")
+
+      expect(photo_info[:data]).to have_key(:attributes)
+      expect(photo_info[:data][:attributes]).to be_a(Hash)
+
+      expect(photo_info[:data][:attributes]).to have_key(:image)
+      expect(photo_info[:data][:attributes][:image]).to be_a(Hash)
+
+      expect(photo_info[:data][:attributes][:image]).to have_key(:location)
+      expect(photo_info[:data][:attributes][:image][:location]).to be_a(String)
+
+      expect(photo_info[:data][:attributes][:image]).to have_key(:image_url)
+      expect(photo_info[:data][:attributes][:image][:image_url]).to be_a(String)
+
+      expect(photo_info[:data][:attributes][:image]).to have_key(:credit)
+      expect(photo_info[:data][:attributes][:image][:credit]).to be_a(Hash)
+
+      expect(photo_info[:data][:attributes][:image][:credit]).to have_key(:source)
+      expect(photo_info[:data][:attributes][:image][:credit][:source]).to be_a(String)
+
+      expect(photo_info[:data][:attributes][:image][:credit]).to have_key(:photographer)
+      expect(photo_info[:data][:attributes][:image][:credit][:photographer]).to be_a(String)
+
+      expect(photo_info[:data][:attributes][:image][:credit]).to have_key(:photographer_profile)
+      expect(photo_info[:data][:attributes][:image][:credit][:photographer_profile]).to be_a(String)
+    end
+
+    it "returns 400 status when missing location param" do
+      get "/api/v1/backgrounds"
+
+      error_info = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.successful?).to eq(false)
+      expect(response.status).to eq(400)
+
+      expect(error_info).to have_key(:error)
+      expect(error_info[:error]).to eq("Please include location param")
+    end
+  end
+end
