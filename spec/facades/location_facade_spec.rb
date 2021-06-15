e6dba367116743a0a213b00a7e069cfb01f86e5b
austinandrade <austinmandrade@gmail.com>
2021-06-15 19:39:11 -0400

    Test location facade get_road_trip happy and sad paths

--- spec/facades/location_facade_spec.rb
+++ spec/facades/location_facade_spec.rb
@@ -13,4 +13,29 @@ RSpec.describe 'LocationFacade' do
       expect(location_object).to be_an_instance_of(Location)
     end
   end
+
+  describe '#get_road_trip' do
+    describe 'happy path' do
+      it 'creates and returns a single roadtrip object', :vcr do
+        start_city      = 'Cleveland, OH'
+        end_city        = 'Denver, CO'
+        forecast_object = Forecast.new(current_weather_data)
+        roadtrip_object = LocationFacade.get_road_trip(start_city, end_city, forecast_object)
+        expect(roadtrip_object).to be_an_instance_of(Roadtrip)
+      end
+    end
+
+    describe 'sad path' do
+      it 'creates and returns a single roadtrip object with impossible travel time and empty weather', :vcr do
+        start_city = 'Cleveland, OH'
+        end_city   = 'London, UK'
+        roadtrip_object  = LocationFacade.get_road_trip(start_city, end_city, bad_location_data)
+        expect(roadtrip_object).to be_an_instance_of(Roadtrip)
+
+        expect(roadtrip_object.travel_time).to eq("impossible")
+        expect(roadtrip_object.weather_at_eta).to eq({})
+        expect(roadtrip_object.weather_at_eta.empty?).to eq(true)
+      end
+    end
+  end
 end
