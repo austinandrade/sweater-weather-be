274af3abcb05fc6cba89af43d374551e6277f6ab
austinandrade <austinmandrade@gmail.com>
2021-06-15 18:51:17 -0400

    Style for verticality

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/forecast', to: 'forecast#show'
-      get '/backgrounds', to: 'backgrounds#show'
-      post '/users', to: 'users#create'
-      post '/sessions', to: 'sessions#create'
-      post '/road_trip', to: 'roadtrip#create'
+      get  '/forecast',    to: 'forecast#show'
+      get  '/backgrounds', to: 'backgrounds#show'
+      post '/users',       to: 'users#create'
+      post '/sessions',    to: 'sessions#create'
+      post '/road_trip',   to: 'roadtrip#create'
     end
   end
 end
