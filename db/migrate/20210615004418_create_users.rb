f8cb6aaf22dafef584204daa1a724e091ac8267a
austinandrade <austinmandrade@gmail.com>
2021-06-14 21:57:00 -0400

    Add users table

--- db/migrate/20210615004418_create_users.rb
+++ db/migrate/20210615004418_create_users.rb
@@ -3,8 +3,11 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :email
       t.string :password_digest
+      t.string :api_key
 
       t.timestamps
     end
+
+    add_index :users, :api_key, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,10 @@ ActiveRecord::Schema.define(version: 2021_06_15_004418) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
+    t.string "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["api_key"], name: "index_users_on_api_key", unique: true
   end
 
 end
