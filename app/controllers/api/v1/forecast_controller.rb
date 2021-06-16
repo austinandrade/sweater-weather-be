ef8cbd377ebf1d85721234222f958c651aed1dc6
austinandrade <austinmandrade@gmail.com>
2021-06-15 21:42:31 -0400

    Return error message if empty location string is passed

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def show
-    if params[:location]
+    if params[:location].present?
       location_coordinates = LocationFacade.get_lat_long(params[:location])
       forecast = ForecastFacade.get_forecast(location_coordinates.latitude, location_coordinates.longitude)
       render json: ForecastSerializer.new(forecast)
