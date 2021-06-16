f77291019319cc5777af3492b8fde3dd5b6bc4d2
austinandrade <austinmandrade@gmail.com>
2021-06-15 21:57:23 -0400

    Return error message if empty location string is passed

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BackgroundsController < ApplicationController
   def show
-    if params[:location]
+    if params[:location].present?
       image = BackgroundsFacade.get_photo(params[:location])
       if image.nil?
         render json: { error: 'No images found with that location. Please try again.' }.to_json, status: :not_found
